4283404c82a6ea0c1a6974608bb5554d5531cb75
Kaden Cyster <kadencyster@gmail.com>
2023-12-27 12:35:33 -0800

    InitialCommit

--- Immortal Empires.py
+++ Immortal Empires.py
@@ -1,4 +1,23 @@
 #Immortal_Empires
+import random
+import math
+#AI_CLASS
+class Peceptron():
+  def __init__(self,input_size):
+    self.weights = [random.uniform(-1,1) for _ in range(input_size)]
+    self.bias = random.uniform(-1,1)
+    self.learning_rate = 0.1
+  def predict(self, inputs):
+    weighted_sum = sum(w * x for w, x in zip(self.weights,inputs)) + self.bias
+    return 1 if weighted_sum > 0 else 0
+  def train(self,training_data,epochs):
+    for epoch in range(epochs):
+      for inputs, target in training_data:
+        prediction = self.predict(inputs)
+        error = target - prediction
+        self.weights = [w + self.learning_rate * error * x for w, x in zip(self.weights,inputs)]
+        self.bias += self.learning_rate * error
+
 
 #CLASSES
 class Empire():
@@ -16,23 +35,28 @@ class Empire():
 
 #VARIBLES & LISTS & ARRAYS
 listOfEmpires = []
-#EmpireName EmpirePosture EmpireTechnology
+#EmpireName EmpirePosture EmpireTechnology EmpireProduction EmpireWarringCapability
 empire1 = Empire(["Empire1",50,1,1,0])
 empire2 = Empire(["Empire2",-50,1,1,0])
-empire3 = Empire(["Empire3",75,1,1,0])
 listOfEmpires.append(empire1)
 listOfEmpires.append(empire2)
-listOfEmpires.append(empire3)
 
-#RUN
+#SETUP_EMPIRE_RELATIONS
 for currentEmpire in range(len(listOfEmpires)):
   for otherEmpire in range(len(listOfEmpires)):
     if listOfEmpires[currentEmpire] != listOfEmpires[otherEmpire]:
       listOfEmpires[currentEmpire].relationSetup(listOfEmpires[otherEmpire].traits)
 
-print(empire1.relations)
-print(empire1.traits)
-print(empire2.relations)
-print(empire2.traits)
-print(empire3.relations)
-print(empire3.traits)
+#ACTUAL_RUN
+training_data = [([0,0],0),
+                 ([0,1],1),
+                 ([1,0],1),
+                 ([1,1],1),]
+peceptron = Peceptron(2)
+peceptron.train(training_data,epochs=100)
+empire1.warshipIncrease
+empire2.warshipIncrease
+print("Empire1Relations:",empire1.relations)
+print("Empire1Traits:",empire1.traits)
+print("Empire2Relations:",empire2.relations)
+print("Empire2Traits:",empire2.traits)
